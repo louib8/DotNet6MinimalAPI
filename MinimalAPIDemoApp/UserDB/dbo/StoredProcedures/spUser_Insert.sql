8a6a82a182b1eca6b4c26ec811fce1cdacfaf952
louib8 <louis.i.brown.8@gmail.com>
2022-01-12 17:59:00 +1100

    Created .NET 6 Minimal API following TimCoreys guide

---
+++ MinimalAPIDemoApp/UserDB/dbo/StoredProcedures/spUser_Insert.sql
@@ -0,0 +1,7 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50)
+AS
+	INSERT INTO dbo.[User] (FirstName, LastName)
+	VALUES (@FirstName, @LastName);
+RETURN 0
